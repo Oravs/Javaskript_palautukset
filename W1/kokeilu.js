ca4c0a2e3a2f4c61bbc7d57fe13892b65691fdba
Oravs <elsa.mustonen@student.laurea.fi>
2025-09-10 18:05:42 +0300

    jee eka kommit

---
+++ W1/kokeilu.js
@@ -0,0 +1,22 @@
+var x=5;
+var y=6;
+var z=x+y;
+console.log(z);
+
+var i = 5; 
+var j = 10;
+var k = 25; 
+var l = 'Joe';
+if (i < j) {
+console.log("i");
+} else if (j > k) {
+console.log("k");
+}else{
+    console.log("l");
+ } 
+
+
+var arr = [3, 5, 7];
+for (var i = 0; i <=3; ++i){
+console.log(arr[i]);
+}
