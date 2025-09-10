22af74829774f94be44908d21c7123a7db8436d6
Oravs <elsa.mustonen@student.laurea.fi>
2025-09-10 18:35:20 +0300

    Korjaa formatointi

--- W1/kokeilu.js
+++ W1/kokeilu.js
@@ -1,22 +1,22 @@
-var x=5;
-var y=6;
-var z=x+y;
+var x = 5;
+var y = 6;
+var z = x + y;
 console.log(z);
 
-var i = 5; 
+var i = 5;
 var j = 10;
-var k = 25; 
+var k = 25;
 var l = 'Joe';
 if (i < j) {
-console.log("i");
+    console.log("i");
 } else if (j > k) {
-console.log("k");
-}else{
+    console.log("k");
+} else {
     console.log("l");
- } 
+}
 
 
 var arr = [3, 5, 7];
-for (var i = 0; i <=3; ++i){
-console.log(arr[i]);
+for (var i = 0; i <= 3; ++i) {
+    console.log(arr[i]);
 }
