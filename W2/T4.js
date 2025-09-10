1ff6bd8f470791178a1bf9040e783a7774f47a79
Oravs <elsa.mustonen@student.laurea.fi>
2025-09-10 21:08:42 +0300

    W2 formating

--- W2/T4.js
+++ W2/T4.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-        var isStudent = confirm("Are you a student?");
+  var isStudent = confirm("Are you a student?");
 
-        var messageContainer = document.getElementById("message");
+  var messageContainer = document.getElementById("message");
 
-        if (isStudent) {
-          messageContainer.innerHTML = "<h2>Welcome student</h2>";
-        } else {
-          messageContainer.innerHTML =
-            "<h2>Sorry, this page is for students only</h2>";
-        }
-      };
+  if (isStudent) {
+    messageContainer.innerHTML = "<h2>Welcome student</h2>";
+  } else {
+    messageContainer.innerHTML =
+      "<h2>Sorry, this page is for students only</h2>";
+  }
+};
--- W2/harjotus2.js
+++ W2/harjotus2.js
@@ -18,15 +18,16 @@ if (navigator.userAgent.indexOf("Firefox") != -1) {
 
 /* T5.1 */
 
-document.write('<img src="http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg" alt="Example Image">');
+document.write(
+  '<img src="http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg" alt="Example Image">'
+);
 
 /* T5.2 */
-var images =[
-				"http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg", 
-				"http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg", 
-				"http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg"
-			];
-		
-var index=Math.floor(Math.random()*images.length);
-document.write(`<img src="${images[index]}" alt="random image">`)
+var images = [
+  "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
+  "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
+  "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg",
+];
 
+var index = Math.floor(Math.random() * images.length);
+document.write(`<img src="${images[index]}" alt="random image">`);
